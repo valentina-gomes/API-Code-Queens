890be6c4d4a2d4d2d631cbd11a910b7b07a4fc11
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2025-09-03 16:53:10 -0300

    endpind POST e GET do array pessoas

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express();
 app.use(express.json());
 
 let pessoas = [
-    {nome: 'leda' ,Idade:14, signo:'virgem'},
+    { id: 1 ,nome: 'leda' , Idade:14, signo:'virgem'},
 
-    { nome: 'valentina' , Idade:13, signo:'aquario'},
+    { id: 2, nome: 'valentina' , Idade:13, signo:'aquario'},
 
-    {nome: 'heitor' ,Idade:15,signo:'virgem'},
+    { id: 3, nome: 'heitor' , Idade:15, signo:'virgem'},
     
-    {nome: 'ana luiza' ,Idade:13,signo:'peixes'},
+    { id: 4, nome: 'ana luiza' , Idade:13, signo:'peixes'},
 
-    { nome: 'luana' ,Idade:39,signo:'capricornio'}
+    { id: 5 ,nome: 'luana' , Idade:39, signo:'capricornio'}
 ]
 
 app.get('/', (req, res) =>{
@@ -25,6 +25,38 @@ app.get('/pessoas', (req, res) =>{
 
 app.get('/pessoas2', (req, res) =>{
     res.json(pessoas[2]);
+});
+
+
+
+
+
+app.post("/pessoas",(req,res) => {
+    const {nome, idade , signo} = req.body;
+    const newUser = {
+        id: pessoas.length +1,
+        nome,
+        idade,
+        signo
+    };
+    pessoas.push(newUser);
+    res.status(201).json(newUser); // codigo de criaçao com sucesso
+}
+)
+
+//get para buscar uma pessoa so por id
+
+app.get('/pessoa/:id', (req, res) =>{
+    
+    const id = parseInt(req.params.id);
+    const pessoa =  pessoas.find( u => u.id === id) ;
+
+    if (!pessoa) {
+        return res.status(484).json({error: "Usuario não encontrado" })
+    }
+    
+    res.json(pessoa);
+
 });
 
 const PORT = 3000;
