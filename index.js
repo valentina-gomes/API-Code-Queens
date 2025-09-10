31fc57277947e8a1bcbf56f858bc94bc2c16b922
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2025-09-10 16:51:47 -0300

    novo crud do tema doce

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/pessoa/:id', (req, res) =>{
     const pessoa =  pessoas.find( u => u.id === id) ;
 
     if (!pessoa) {
-        return res.status(484).json({error: "Usuario não encontrado" })
+        return res.status(404).json({error: "Usuario não encontrado" })
     }
     
     res.json(pessoa);
@@ -60,6 +60,13 @@ app.get('/pessoa/:id', (req, res) =>{
 
 });
 
+// req => requisiçao | res => resposta
+app.get('/totalPessoas' , (req , res) => {
+
+    console.log("REQUISICAO:" ,req);
+   console.log("/n/nRESPONSE: " ,res)
+  res.json(pessoas.length);
+});
 
 
 app.put('/pessoa2/:id' , (req, res) => {
@@ -67,7 +74,7 @@ app.put('/pessoa2/:id' , (req, res) => {
     const pessoa =  pessoas.find( u => u.id === id) ;
 
     if (!pessoa) {
-        return res.status(484).json({error: "Usuario não encontrado" })
+        return res.status(404).json({error: "Usuario não encontrado" })
     }
     
 
@@ -86,12 +93,13 @@ app.put('/pessoa2/:id' , (req, res) => {
 
 });
 
+
 app.delete("/removerPessoa/:id" , (res , req) => {
     const id = parseInt(req.params.id);
     const pessoa =  pessoas.find( u => u.id === id) ;
 
     if (!pessoa) {
-        return res.status(484).json({error: "Usuario não encontrado" })
+        return res.status(404).json({error: "Usuario não encontrado" })
     }
 
 
@@ -100,6 +108,113 @@ console.log(pessoas) ;
 res.json(pessoas);
 
 })
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let doces = [
+    { id: 1 ,nome: 'brigadeiro' , peso:'14g', formato:'bola' , validade: 2026},
+
+    { id: 2 ,nome: 'bolo de morango' , peso:'500g', formato:'retangular' , validade: 2026},
+
+    { id: 3 ,nome: 'cookie' , peso:'70g', formato:'redondo' , validade: 2026},
+    
+    { id: 4 ,nome: 'gelatina' , peso:'300g', formato:'retangular' , validade: 2026},
+
+    { id: 5 ,nome: 'croissaint' , peso:'100g', formato:'meia lua' , validade: 2026},
+]
+
+
+app.get('/doce' , (req , res) => {
+  if(doces.length === 0){
+    res.status(404).json({
+    status  : 404 ,
+    error : "NOT_FOUND" ,
+    message : "informaçao nao encontrada"
+    
+    })
+}
+
+    res.json(doces);
+});
+
+
+app.get('/doce/:id', (req, res) =>{
+    
+    const id = parseInt(req.params.id);
+    const doce =  doces.find( u => u.id === id) ;
+
+    if (!doce) {
+        return res.status(484).json({error: "Usuario não encontrado" })
+    }
+    
+    res.json(doce);
+
+
+});
+
+
+app.put('/doce/:id' , (req, res) => {
+    const id = parseInt(req.params.id);
+    const doce =  doces.find( u => u.id === id) ;
+
+    if (!doce) {
+        return res.status(484).json({error: "Usuario não encontrado" })
+    }
+    
+
+
+    const {nome, peso , formado, validade} = req.body;
+
+     novoDoce= req.body;
+     console.log("doce antigo : " ,doce)
+     console.log("doce novo : " ,novoDoce)
+
+    doce.nome=novoDoce.nome || doce.nome
+    doce.peso=novoDoce.peso || doce.peso
+    doce.formato=novoDoce.formato || doce.formato
+    doce.validade=novoDoce.validade || doce.validade
+
+
+
+     doces[doce.id - 1] = {...doce , id: doce.id}
+     console.log("doces: " ,doces);
+    res.json(doces);
+
+});
+
+
+app.post("/doce",(req,res) => {
+    const {nome, peso , formato, validade } = req.body;
+    const novoDoce = {
+        id: doces.length +1,
+        nome, 
+        peso ,
+        formato,
+        validade
+    };
+    pessoas.push(novoDoce);
+    res.status(201).json(novoDoce); // codigo de criaçao com sucesso
+});
+
+app.delete("/doce/:id" , (res , req) => {
+    const id = parseInt(req.params.id);
+    const doce =  doces.find( (u) => u.id === id) ;
+
+    if (!doce) {
+        return res.status(404).json({error: "Usuario não encontrado" })
+    }
+
+doces.splice(doce.id - 1 , 1) ;
+console.log(doces) ;
+res.json(doces);
+
+});
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
